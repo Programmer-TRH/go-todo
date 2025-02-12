caf170e937b38462a2ebe43671b2c774dc9c658e
Programmer-TRH <tokdirrahoman4@gmail.com>
2025-02-12 21:22:31 +0600

    Mongo DB Fixed connection

--- config/database.go
+++ config/database.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,15 +15,24 @@ func ConnectDB() {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	clinetOptions := options.Client().ApplyURI("mongodb+srv://tokdirrahoman4:01833740078Trh@todo-go.qksdh.mongodb.net/?retryWrites=true&w=majority&appName=Todo-GO").SetServerAPIOptions(serverAPI)
 	client, err := mongo.Connect(context.TODO(), clinetOptions)
+
 	if err != nil {
 		log.Fatal("❌ MongoDB Connection Failed:", err)
 	}
 
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
+	// Ensure disconnection when the app shuts down
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Println("⚠️ MongoDB Disconnect Failed:", err)
+		}
+	}()
+
+	// Ping to check connection
+	if err := client.Ping(context.TODO(), nil); err != nil {
 		log.Fatal("❌ MongoDB Ping Failed:", err)
 	}
 
+	// Set database & collection
 	DB = client.Database("Todo-GO").Collection("todos")
-	log.Println("✅ Connected to MongoDB!")
+	fmt.Println("✅ Connected to MongoDB!")
 }
